8eb53b5b76b586636c970f886410442e835c9bb0
DIMON710 <55649421+DIMON710@users.noreply.github.com>
2023-06-18 23:08:02 +0300

    i_don't-know

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 const TasksControllers = require('./controllers/tasksControllers')
-const http = require("http");
 const PORT = process.env.PORT || 5000;
 const CLIENT = process.env.CLIENT;
 const ssl = require('ssl-express-www');
@@ -29,7 +28,7 @@ app.use(express.json());
 app.use('/api', router);
 app.use(errorHandler);
 
-const server = http.Server(app);
+const server = require("http").Server(app);
 const io = socketIO(server, {
     cors: {
         origin: CLIENT,
@@ -59,7 +58,7 @@ const startApp = async () => {
     try {
         await sequelize.authenticate();
         await sequelize.sync();
-        server.listen(PORT,() => console.log('SERVER WORKING ON PORT ' + PORT));
+        server.listen(PORT, () => console.log('SERVER WORKING ON PORT ' + PORT));
     } catch (e) {
         console.log(e);
     }
